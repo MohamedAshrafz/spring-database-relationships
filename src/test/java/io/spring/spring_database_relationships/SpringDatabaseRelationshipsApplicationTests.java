ca2cf13148e436a702bac38422795d11aab9b895
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2025-05-29 00:17:36 +0300

    Init commit

---
+++ src/test/java/io/spring/spring_database_relationships/SpringDatabaseRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.spring_database_relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabaseRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
